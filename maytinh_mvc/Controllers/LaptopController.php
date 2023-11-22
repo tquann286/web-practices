d0d253afce8dc32fef5f169867c7be93fbc1f2e8
tquann286 <quantrung286@gmail.com>
2023-11-22 14:28:42 +0700

    fix

--- maytinh_mvc/Controllers/LaptopController.php
+++ maytinh_mvc/Controllers/LaptopController.php
@@ -8,14 +8,20 @@ class LaptopController {
         $link = null;
         taoKetNoi($link);
 
+
         $query = "select * from tbl_maytinh";
         $result = chayTruyVanTraVeDL($link, $query);
 
+        
         while ($row = mysqli_fetch_assoc($result)) {
+
             $maytinh = new Laptop($row['id'], $row['ten'], $row['gia'], $row['anh']);
 
+
             echo "<div class='col-md-4'>";
+
             echo "<div class='card'>";
+            
             echo "<img src='{$maytinh->getAnh()}' class=' img-fluid' alt='Image'>";
             echo "<div class='card-body'>";
             echo "<h5 class='card-title'>{$maytinh->getTen()}</h5>";
